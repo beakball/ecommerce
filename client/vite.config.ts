ad6e6bd489649e0a93bb93e7d253b66aa1cd6f07
Sadid Alam <sadidalam14@gmail.com>
2025-07-13 17:05:12 +0530

    added backend url to vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "https://ecom-beak.onrender.com",
         changeOrigin: true,
         secure: false,
       },
